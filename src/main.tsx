514a1db75a575da071c74542c1f8c4e5d2679eee
KEVIN ALEJANDRO ALDRETE MARTINEZ <kevin.aldrete1@outlook.com>
2024-12-22 16:11:23 -0600

    change aproved

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
+import Router from './router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <h1>Hola Mundo</h1>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router />
+    <h1 className="text-6xl">Hola Mundo</h1>
+  </React.StrictMode>
 )
+
