34e9941cdf6ace772846eb1048718ee9bde3f5cd
PabloHernandez03 <pablohernandez6287@alumnos.udg.mx>
2025-01-13 22:11:54 -0600

    Responsive PedidosView

--- src/views/PedidosView.tsx
+++ src/views/PedidosView.tsx
@@ -25,10 +25,10 @@ const PedidosView: React.FC = () => {
 
   return (
     <div>
-        <div className="flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
+        <div className="md:flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
           <video
             src={resources[1].src}
-            className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+            className="w-3/4 mb-4 md:mb-0 md:w-1/5 rounded-lg shadow-lg border-2 border-white-900"
             autoPlay
             loop
             muted
@@ -37,21 +37,21 @@ const PedidosView: React.FC = () => {
             <img
               src={resources[0].src}
               alt="Pedido 1"
-              className="w-3/4 rounded-lg shadow-lg border-2 border-white-900" 
+              className="w-full md:w-3/4 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900" 
             />
             </div>
           <video
             src={resources[2].src}
-            className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+            className="w-3/4 md:w-1/5 rounded-lg shadow-lg border-2 border-white-900"
             autoPlay
             loop
             muted
           />
         </div>
-        <div className="flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
+        <div className="md:flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
           <video
               src={resources[3].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 mb-4 md:mb-0 md:w-1/5 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
@@ -60,12 +60,12 @@ const PedidosView: React.FC = () => {
               <img
                 src={resources[4].src}
                 alt="Pedido 2"
-                className="w-3/4 rounded-lg shadow-lg border-2 border-white-900"
+                className="w-full md:w-3/4 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
               />
             </div>
             <video
               src={resources[5].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
@@ -74,17 +74,17 @@ const PedidosView: React.FC = () => {
         <div className='mb-10'>
           <h2 className='text-4xl text-center font-bold text-[#F7F6F6]'>"La mejor calidad, al mejor precio"</h2>
         </div>
-        <div className="flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
+        <div className="md:flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
             <div className="flex justify-center items-center w-full">
               <img
                 src={resources[7].src}
                 alt="Pedido 3"
-                className="w-10/12 rounded-lg shadow-lg border-2 border-white-900"
+                className="w-full md:w-10/12 mb-4 md:mb-0  rounded-lg shadow-lg border-2 border-white-900"
               />
             </div>
           <video
               src={resources[6].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
@@ -93,44 +93,44 @@ const PedidosView: React.FC = () => {
               <img
                 src={resources[8].src}
                 alt="Pedido 4"
-                className="w-10/12 rounded-lg shadow-lg border-2 border-white-900"
+                className="w-full md:w-10/12 rounded-lg shadow-lg border-2 border-white-900"
               />
             </div>
         </div>
-        <div className="flex justify-around items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
+        <div className="md:flex justify-items-center md:justify-around items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
           <video
               src={resources[9].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
             />
             <video
               src={resources[10].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
             />
             <video
               src={resources[11].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
             />
         </div>
-        <div className="flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
+        <div className="md:flex justify-items-center items-center mb-10 px-10 py-8 rounded-xl border-2 border-white-900">
           <div className="flex justify-center items-center w-full">
             <img
               src={resources[12].src}
               alt="Pedido 5"
-              className="w-10/12 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-full md:w-10/12  mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
             />
           </div>
           <video
               src={resources[13].src}
-              className="w-1/5 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-3/4 md:w-1/5 mb-4 md:mb-0 rounded-lg shadow-lg border-2 border-white-900"
               autoPlay
               loop
               muted
@@ -139,7 +139,7 @@ const PedidosView: React.FC = () => {
             <img
               src={resources[14].src}
               alt="Pedido 6"
-              className="w-10/12 rounded-lg shadow-lg border-2 border-white-900"
+              className="w-full md:w-10/12 rounded-lg shadow-lg border-2 border-white-900"
             />
           </div>
         </div>
