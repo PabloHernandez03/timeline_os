42a7bb00f7c071c2fd941894c599618c6b02efa3
PabloHernandez03 <pablohernandez6287@alumnos.udg.mx>
2025-02-01 15:24:18 -0600

    SEO improved

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,6 +6,10 @@ export default function AppLayout() {
             <section>
                 <Outlet />
             </section>
+            <footer className="bg-gray-800 text-white text-center py-4">
+                <p><span className="font-bold">Publicación:</span> 01 de febrero de 2025</p>
+                <p><span className="font-bold">Autor:</span> Pablo Alejandro Hernández Gómez</p>
+            </footer>
         </>
     )
 }
